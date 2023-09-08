4802dc142e632d27c5bc6dac3002eba83614c373
redpalico <red.palico@outlook.com>
2023-09-08 16:45:49 +0900

    update jisho

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
 
-# Create a list of terrains
-terrains = ["G", "W", "F", "D", "M"]
+# Define the terrains and their corresponding alphabets
+terrains = {
+    "G": "Grassland",  # 草原
+    "W": "Water",      # 水
+    "F": "Forest",     # 森
+    "D": "Desert",     # 砂漠
+    "M": "Mountain"    # 山
+}
 
-# Create a 6x6 grid
-grid = [[random.choice(terrains) for _ in range(6)] for _ in range(6)]
+# Create a 6x6 grid with random terrains
+grid = [[random.choice(list(terrains.keys())) for _ in range(6)] for _ in range(6)]
 
 # Display the grid as text
 for row in grid:
