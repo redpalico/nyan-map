0cff0fd433b6f50866b5dbf1f28bf4dcfcb32ac9
redpalico <red.palico@outlook.com>
2023-09-08 16:55:30 +0900

    update water

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import random
 
 # Define the terrains and their corresponding alphabets
 terrains = {
-    "G": "Grassland",  # 草原
-    "W": "Water",      # 水
-    "F": "Forest",     # 森
-    "D": "Desert",     # 砂漠
-    "M": "Mountain"    # 山
+    "G": "Grassland",
+    "W": "Water",
+    "F": "Forest",
+    "D": "Desert",
+    "M": "Mountain"
 }
 
 # Create a 6x6 grid with random terrains
 grid = [[random.choice(list(terrains.keys())) for _ in range(6)] for _ in range(6)]
 
-# Display the grid as text
+# Replace "W" with "*" to indicate water terrains distinctly
+for i in range(6):
+    for j in range(6):
+        if grid[i][j] == "W":
+            grid[i][j] = "*"
+
+# Display the grid as text with spaces between each character
 for row in grid:
-    print("".join(row))
+    print(" ".join(row))
